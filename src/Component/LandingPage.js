948f008fa73f39c917bc0bb373ace6679d07a049
jyothi <jyothirlathaseelam1234@gmail.com>
2024-08-30 14:13:15 +0530

    first commit

---
+++ src/Component/LandingPage.js
@@ -0,0 +1,157 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+
+function LandingPage() {
+  return (
+    <div className="box">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-4  col-md-4">
+            <div className="box-center">
+              <Card
+                style={{
+                  width: "18rem",
+                  backgroundColor: "pink",
+                  margin: "40px 80px 30px 20px",
+                  padding: "20px 20px ",
+                }}
+              >
+                <Card.Body>
+                  <Card.Title>instagram</Card.Title>
+                  <Card.Text>
+                    dhyruri hujeje hduhd fdfgjk cfcgh cfgvhj cfgvhbj cfgvhbj
+                    fgyhj fghj cgvhb dfdyhj xftgyhuijolk; esdrtfyuiuo xdfgyhuk
+                    ftgyhuk xxdftugyhu dftugyhuk
+                  </Card.Text>
+                  <Card.Link href="#">learn more</Card.Link>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+
+          <div className="col-xs-4  col-md-4">
+            <div className="box-center">
+              <Card
+                style={{
+                  width: "18rem",
+                  backgroundColor: "blue",
+                  margin: "40px 80px 30px 20px",
+                  padding: "20px 20px "
+                }}
+              >
+                <Card.Body>
+                  <Card.Title>Facebook</Card.Title>
+                  <Card.Text>
+                    dhyruri hujeje hduhd fdfgjk cfcgh cfgvhj cfgvhbj cfgvhbj
+                    fgyhj fghj cgvhb dfdyhj xftgyhuijolk; esdrtfyuiuo xdfgyhuk
+                    ftgyhuk xxdftugyhu dftugyhuk
+                  </Card.Text>
+                  <Card.Link href="#">learn more</Card.Link>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+
+          <div className="col-xs-4  col-md-4">
+            <div className="box-center">
+              <Card
+                style={{
+                  width: "18rem",
+                  backgroundColor: "red",
+                  margin: "40px 80px 30px 20px",
+                  padding: "20px 20px ",
+
+                }}
+              >
+                <Card.Body>
+                  <Card.Title>Twitter</Card.Title>
+                  <Card.Text>
+                    dhyruri hujeje hduhd fdfgjk cfcgh cfgvhj cfgvhbj cfgvhbj
+                    fgyhj fghj cgvhb dfdyhj xftgyhuijolk; esdrtfyuiuo xdfgyhuk
+                    ftgyhuk xxdftugyhu dftugyhuk
+                  </Card.Text>
+                  <Card.Link href="#">learn more</Card.Link>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+          <div className="col-xs-4  col-md-4">
+            <div className="box-center">
+              <Card
+                style={{
+                  width: "18rem",
+                  backgroundColor: "green",
+                  margin: "40px 80px 30px 20px",
+                  padding: "20px 20px ",
+
+                }}
+              >
+                <Card.Body>
+                  <Card.Title>Whatsup</Card.Title>
+                  <Card.Text>
+                    dhyruri hujeje hduhd fdfgjk cfcgh cfgvhj cfgvhbj cfgvhbj
+                    fgyhj fghj cgvhb dfdyhj xftgyhuijolk; esdrtfyuiuo xdfgyhuk
+                    ftgyhuk xxdftugyhu dftugyhuk
+                  </Card.Text>
+                  <Card.Link href="#">learn more</Card.Link>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+
+          <div className="col-xs-4  col-md-4">
+            <div className="box-center">
+              <Card
+                style={{
+                  width: "18rem",
+                  backgroundColor: "lightgreen",
+                  margin: "40px 80px 30px 20px",
+                  padding: "20px 20px ",
+                }}
+              >
+                <Card.Body>
+                  <Card.Title>Snap</Card.Title>
+                  <Card.Text>
+                    dhyruri hujeje hduhd fdfgjk cfcgh cfgvhj cfgvhbj cfgvhbj
+                    fgyhj fghj cgvhb dfdyhj xftgyhuijolk; esdrtfyuiuo xdfgyhuk
+                    ftgyhuk xxdftugyhu dftugyhuk
+                  </Card.Text>
+                  <Card.Link href="#">learn more</Card.Link>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+
+          <div className="col-xs-4  col-md-4">
+            <div className="box-center">
+              <Card
+                style={{
+                  width: "18rem",
+                  backgroundColor: "pink",
+                  margin: "40px 80px 30px 20px",
+                  padding: "20px 20px ",
+
+                }}
+              >
+                <Card.Body>
+                  <Card.Title>Phonepe</Card.Title>
+                  <Card.Text>
+                    dhyruri hujeje hduhd fdfgjk cfcgh cfgvhj cfgvhbj cfgvhbj
+                    fgyhj fghj cgvhb dfdyhj xftgyhuijolk; esdrtfyuiuo xdfgyhuk
+                    ftgyhuk xxdftugyhu dftugyhuk
+                  </Card.Text>
+                  <Card.Link href="#">learn more</Card.Link>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
